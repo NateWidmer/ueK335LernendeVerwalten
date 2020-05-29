36259a28c713e96d6abd79ea402ddef33a91cbf4
Salome Treichler <salome.treichler@noseryoung.ch>
2020-05-29 17:03:39 +0200

    first tries

--- app/src/main/java/ch/noseryoung/lernendeverwaltung/CreateActivity.java
+++ app/src/main/java/ch/noseryoung/lernendeverwaltung/CreateActivity.java
@@ -1,12 +1,16 @@
 package ch.noseryoung.lernendeverwaltung;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class CreateActivity extends AppCompatActivity {
 
@@ -24,7 +28,21 @@ public class CreateActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
         spinner = findViewById(R.id.companySpinner);
         spinner.setAdapter(adapter);
+
+        FloatingActionButton addButton = findViewById(R.id.saveButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
     }
+
+    private void openMainActivity() {
+        Intent intend = new Intent(this, MainActivity.class);
+        startActivity(intend);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/ch/noseryoung/lernendeverwaltung/MainActivity.java
+++ app/src/main/java/ch/noseryoung/lernendeverwaltung/MainActivity.java
@@ -6,22 +6,37 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 import android.widget.SearchView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<User> data = new ArrayList<User>();
+    FloatingActionButton addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         data.add(new User("Gianluca", "Daffré"));
         data.add(new User("Peter", "Meier"));
         data.add(new User("Hans", "Muster"));
+
+        addButton = findViewById(R.id.addButton);
+        addButton.bringToFront();
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCreateActivity();
+            }
+        });
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RecyclerView userView = findViewById(R.id.avatarList);
@@ -31,6 +46,15 @@ public class MainActivity extends AppCompatActivity {
         userView.setLayoutManager(linearLayoutManager);
         UserAdapter userAdapter = new UserAdapter(data);
         userView.setAdapter(userAdapter);
+
+
+
+
+    }
+
+    private void openCreateActivity() {
+        Intent intend = new Intent(this, CreateActivity.class);
+        startActivity(intend);
     }
 
     @Override
