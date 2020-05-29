53b23274a5983e3c65d21bb75468b1a83c578be5
Salome Treichler <salome.treichler@noseryoung.ch>
2020-05-29 13:36:14 +0200

    finished Profile picture and form

--- app/src/main/java/ch/noseryoung/lernendeverwaltung/CreateActivity.java
+++ app/src/main/java/ch/noseryoung/lernendeverwaltung/CreateActivity.java
@@ -1,14 +1,26 @@
 package ch.noseryoung.lernendeverwaltung;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateActivity extends AppCompatActivity {
+
+    Spinner spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setTitle(R.string.create_title);
         setContentView(R.layout.activity_create);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,
+                R.array.companies, R.layout.spinner_item);
+
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        spinner = findViewById(R.id.spinner);
+        spinner.setAdapter(adapter);
     }
 }
