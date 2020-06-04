edc5df22cb1bd2baea5618be0579ca369ffaad39
Salome Treichler <salome.treichler@noseryoung.ch>
2020-06-04 15:18:36 +0200

    Detail Activity refactored and outsourced functions

--- app/src/main/java/ch/noseryoung/lernendeverwaltung/DetailActivity.java
+++ app/src/main/java/ch/noseryoung/lernendeverwaltung/DetailActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import ch.noseryoung.lernendeverwaltung.model.User;
